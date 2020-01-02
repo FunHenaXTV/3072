56d3599bdead38d22424d8ff892ec0f2ee13e1d3
Dmitry Zubkov <dz2003@bk.ru>
2020-01-02 18:03:28 +0300

    Change structure of programm

--- main.py
+++ main.py
@@ -10,31 +10,41 @@ class Desk:
         self.texts = []
         self.points = []
         k = 0
+
+        empty_list = [0, 0, 0, 0]
+        self.objects = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        self.texts = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        i = 0
+        j = 0
         for y in range(2, 502, 125):
             for x in range(2, 502, 125):
                 self.points = [x, y, x, y+125, x+125, y+125, x+125, y]
-                self.objects.append(canvas.create_polygon(self.points, outline="#bbb09e", fill="#bbb09e"))
-                self.texts.append(canvas.create_text(62.5+x, 62.5+y, text='', font=30))
-
-        x1 = randint(0, 15)
-        y1 = randint(0, 15)
-        while x1 == y1:
+                self.objects[i][j] = (canvas.create_polygon(self.points, outline="#bbb09e", fill="#bbb09e"))
+                self.texts[i][j] = (canvas.create_text(62.5+x, 62.5+y, text=str(i)+str(j), font=30))
+                i += 1
+            j += 1
+            i = 0
+        x1 = randint(0, 3)
+        x12 = randint(0, 3)
+        y1 = randint(0, 3)
+        y12 = randint(0, 3)
+        while x1 == y1 and y12 == x12:
             x1 = randint(0, 15)
 
         x2 = randint(1, 3)
         y2 = randint(1, 3)
         if x2 == 1 or x2 == 2:
-            canvas.itemconfig(self.texts[x1], text='3')
+            canvas.itemconfig(self.texts[x1][x12], text='3')
         else:
-            canvas.itemconfig(self.texts[x1], text='6')
+            canvas.itemconfig(self.texts[x1][x12], text='6')
 
         if y2 == 1 or y2 == 2:
-            canvas.itemconfig(self.texts[y1], text='3')
+            canvas.itemconfig(self.texts[y1][y12], text='3')
         else:
-            canvas.itemconfig(self.texts[y1], text='6')
+            canvas.itemconfig(self.texts[y1][y12], text='6')
  
-        self.check_color(x1)
-        self.check_color(y1)
+        self.check_color(x1, x12)
+        self.check_color(y1, y12)
 
 
     def check_event(self, event):
@@ -49,7 +59,9 @@ class Desk:
             self.Left()
 
     def Up(self):
-        pass
+        for i in range(4, 16):
+            if canvas.itemcget(self.texts[i], 'text') != '':
+                pass
 
     def Down(self):
         pass
@@ -61,33 +73,33 @@ class Desk:
         pass
 
 
-    def check_color(self, i):
-        if canvas.itemcget(self.texts[i], 'text') == '3':
-            canvas.itemconfig(self.objects[i], fill='#eee4da', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '6':
-            canvas.itemconfig(self.objects[i], fill='#eedfc8', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '':
-            canvas.itemconfig(self.objects[i], fill='#bbb09e', outline="#bbb09e")
-        elif canvas.itemcget(self.texts[i], 'text') == '12':
-            canvas.itemconfig(self.objects[i], fill='#f3b079', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '24':
-            canvas.itemconfig(self.objects[i], fill='#f59563', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '48':
-            canvas.itemconfig(self.objects[i], fill='#f97c60', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '96':
-            canvas.itemconfig(self.objects[i], fill='#f85e3a', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '192':
-            canvas.itemconfig(self.objects[i], fill='#edce73', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '384':
-            canvas.itemconfig(self.objects[i], fill='#edcb60', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '768':
-            canvas.itemconfig(self.objects[i], fill='#eec750', outline="#fff")            
-        elif canvas.itemcget(self.texts[i], 'text') == '1536':
-            canvas.itemconfig(self.objects[i], fill='#ecc441', outline="#fff")
-        elif canvas.itemcget(self.texts[i], 'text') == '3072':
-            canvas.itemconfig(self.objects[i], fill='#edc12c', outline="#fff")
+    def check_color(self, i, j):
+        if canvas.itemcget(self.texts[i][j], 'text') == '3':
+            canvas.itemconfig(self.objects[i][j], fill='#eee4da', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '6':
+            canvas.itemconfig(self.objects[i][j], fill='#eedfc8', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '':
+            canvas.itemconfig(self.objects[i][j], fill='#bbb09e', outline="#bbb09e")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '12':
+            canvas.itemconfig(self.objects[i][j], fill='#f3b079', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '24':
+            canvas.itemconfig(self.objects[i][j], fill='#f59563', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '48':
+            canvas.itemconfig(self.objects[i][j], fill='#f97c60', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '96':
+            canvas.itemconfig(self.objects[i][j], fill='#f85e3a', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '192':
+            canvas.itemconfig(self.objects[i][j], fill='#edce73', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '384':
+            canvas.itemconfig(self.objects[i][j], fill='#edcb60', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '768':
+            canvas.itemconfig(self.objects[i][j], fill='#eec750', outline="#fff")            
+        elif canvas.itemcget(self.texts[i][j], 'text') == '1536':
+            canvas.itemconfig(self.objects[i][j], fill='#ecc441', outline="#fff")
+        elif canvas.itemcget(self.texts[i][j], 'text') == '3072':
+            canvas.itemconfig(self.objects[i][j], fill='#edc12c', outline="#fff")
         else:
-            canvas.itemconfig(self.objects[i], fill='#fc3f3d')
+            canvas.itemconfig(self.objects[i][j], fill='#fc3f3d')
 
 
 
